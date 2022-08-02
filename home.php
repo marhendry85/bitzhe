db769247520136b9fc5f5e62bdee97109d6df3a3
marhendry85 <99333036+marhendry85@users.noreply.github.com>
2022-08-02 18:58:12 +0700

    home.php

---
+++ home.php
@@ -0,0 +1,7 @@
+echo "# bitzhe" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/marhendry85/bitzhe.git
+git push -u origin main
